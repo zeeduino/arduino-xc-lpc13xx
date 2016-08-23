975ed45642f14bf3883bb56b27d5e61372effd0a
Nidal P <nidalp@arduino-xc.com>
2016-08-23 11:44:33 -0500

    Fast pin handle init function implementation.

---
+++ src/board/board_fast_pins.c
@@ -0,0 +1,19 @@
+
+#include <inttypes.h>
+#include "chip.h"
+
+#include "pins_arduino.h"
+
+// this goes to HAL since internal pin structures are only accessible there
+void Board_Fast_Pins_Handle_Init(volatile uint32_t **pinSetHandle,
+		volatile uint32_t **pinClrHandle,
+		volatile uint32_t **pinToggleHandle,
+		volatile uint32_t *pinPositionHandle,
+		int arduinoPin)
+{
+	*pinSetHandle = &((DIG_MAP_PORT(arduinoPin))->SET[APIN_PORT(arduinoPin)]);
+	*pinClrHandle = &((DIG_MAP_PORT(arduinoPin))->CLR[APIN_PORT(arduinoPin)]);
+	*pinToggleHandle = &((DIG_MAP_PORT(arduinoPin))->NOT[APIN_PORT(arduinoPin)]);
+
+	*pinPositionHandle = (1UL << APIN_PIN(arduinoPin));
+}
