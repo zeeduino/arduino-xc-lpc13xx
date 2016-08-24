ebe9e7311eb7b4f4064f5f0d9d3de6f356a75739
Nidal P <nidalp@arduino-xc.com>
2016-08-23 22:30:15 -0500

    added fast pin read function

--- src/board/board_fast_pins.c
+++ src/board/board_fast_pins.c
@@ -5,14 +5,17 @@
 #include "pins_arduino.h"
 
 // this goes to HAL since internal pin structures are only accessible there
-void Board_Fast_Pins_Handle_Init(volatile uint32_t **pinSetHandle,
+void Board_Fast_Pins_Handle_Init(
+		volatile uint32_t **pinSetHandle,
 		volatile uint32_t **pinClrHandle,
+		volatile uint32_t **pinReadHandle,
 		volatile uint32_t **pinToggleHandle,
 		volatile uint32_t *pinPositionHandle,
 		int arduinoPin)
 {
 	*pinSetHandle = &((DIG_MAP_PORT(arduinoPin))->SET[APIN_PORT(arduinoPin)]);
 	*pinClrHandle = &((DIG_MAP_PORT(arduinoPin))->CLR[APIN_PORT(arduinoPin)]);
+	*pinReadHandle = &((DIG_MAP_PORT(arduinoPin))->PIN[APIN_PORT(arduinoPin)]);
 	*pinToggleHandle = &((DIG_MAP_PORT(arduinoPin))->NOT[APIN_PORT(arduinoPin)]);
 
 	*pinPositionHandle = (1UL << APIN_PIN(arduinoPin));
